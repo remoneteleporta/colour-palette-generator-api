cd9d999bca179db3515cc4954aa18f55e9105a5e
Remone Teleporta <remoneteleporta@gmail.com>
2025-11-16 23:36:35 +0530

    hex colour input JS added, styles for colour columns added, colour palette display section added in HTML

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-const colourPick = document.getElementById("colour-picker")
-const seedhexInput = document.getElementById("seed-colour-hex")
+const seedColourInput = document.getElementById("seed-colour-input")
+const seedHexInput = document.getElementById("seed-colour-hex")
+
+/* Getting Seed Colour Input as Hex Code */
+
+seedHexInput.addEventListener("input", function(){
+     if (/^#([0-9A-F]{6})$/i.test(seedHexInput.value)) {
+        seedColourInput.value = seedHexInput.value;
+    }
+})
+
+seedColourInput.addEventListener("input", function(){
+    seedHexInput.value = seedColourInput.value.toUpperCase();
+});
 
 fetch("https://www.thecolorapi.com/scheme?hex=24B1E0&mode=triad&count=6")
 .then(resp => resp.json())
